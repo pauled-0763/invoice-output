035b2eb3569b0d23506d231be643a4d8ad5651ba
pauled-0763 <nganga.njuguna23@students.dkut.ac.ke>
2024-10-02 17:09:09 +0300

    Add files via upload

---
+++ InvoiceOutput.java
@@ -0,0 +1,34 @@
+public class InvoiceOutput {
+    public static void main(String[] args) {
+        System.out.println("#99 C026-01-0736/2023 Building Gilmore Ave, New Mla., Quezon City     Tel. No:123 4567");
+        System.out.println("          VAT REG. TIN 098-765-4321-0000");
+        System.out.println("           SALES INVOICE");
+        System.out.println();
+        System.out.println("Sold to: Paul Njuguna Ng'ang'a                  Date:28/04/2020");
+        System.out.println("Address: private bag                          Contact Number: 0100100100");
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Qty  |  Item Description  |  Unit Price  |  Amount");
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("4    |  Pencil            |  20.00       |  80.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("10   |  Duster            |  50.00       |  500.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("9    |  Pens              |  30.00       |  270.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("10   |  Crayon            |  80.00       |  800.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("                               Sub Total:  1650.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("                               VAT(16%):   264.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+        System.out.println("                             Total Amount: 1914.00");
+        System.out.println("-----------------------------------------------");
+        System.out.println("-----------------------------------------------");
+}
+}
